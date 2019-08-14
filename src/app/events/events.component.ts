188c0e8daf8de7b4a89ed93ef942a644a5af7b4f
hpitawela <hpitawela@uiowa.edu>
2019-08-13 20:46:19 -0500

    src path changed for images in events

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./events.component.css','../app.component.css']
 })
 export class EventsComponent implements OnInit {
-  IMG1 = "assets/events-img/events-cover.jpg";
-  IMG2 = "assets/events-img/2.jpg";
 
   constructor() { }
 
